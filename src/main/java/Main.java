64df702be9aa5de6efe2c906e4abcaef8938549c
Cauan Josias Moreira <168428906+CauanJMoreira@users.noreply.github.com>
2024-09-02 12:15:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("insira as coordenadas");
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    if (x > 0 && y > 0)
+    
+  }
+
+  sc.close();
+}
